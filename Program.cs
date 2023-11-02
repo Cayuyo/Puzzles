d619e4826376220c380f64a81856bc3fe98a1c78
Cayuyo <cayuken00@gmail.com>
2023-11-02 02:14:17 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("********** Matriz Aleatoria **********");
+int[] randomNumbers = RandomArray();
+
+string formattedList = "Lista de números: {" + string.Join(", ", randomNumbers) + "}";
+Console.WriteLine(formattedList);
+
+Console.WriteLine();
+
+int minValue = int.MaxValue;
+int maxValue = int.MinValue;
+
+foreach (int number in randomNumbers)
+{
+    if (number < minValue)
+    {
+        minValue = number;
+    }
+    if (number > maxValue)
+    {
+        maxValue = number;
+    }
+}
+Console.WriteLine("Valor mínimo: {0}", minValue);
+Console.WriteLine("Valor máximo: {0}", maxValue);
+
+int sum = 0;
+foreach (int number in randomNumbers)
+{
+    sum += number;
+}
+
+Console.WriteLine("Suma de todos los valores: {0}", sum);
+
+static int[] RandomArray()
+{
+    Random random = new();
+    int[] array = new int[10];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(5, 26);
+    }
+
+    return array;
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("********** Lanzamiento de Moneda **********");
+
+static string TossCoin()
+{
+    Console.WriteLine("Lanzando una moneda!");
+
+    Random random = new Random();
+    int randomNumber = random.Next(2);
+    if (randomNumber == 0)
+    {
+        return "Cara";
+    }
+    else
+    {
+        return "Cruz"; // Dato: En Chile se le conoce como "Sello".
+    }
+}
+
+static double TossMultipleCoins(int num)
+{
+    int headsCount = 0;
+
+    for (int i = 0; i < num; i++)
+    {
+        string result = TossCoin();
+        if (result == "Cara")
+        {
+            headsCount++;
+        }
+    }
+
+    double ratio = (double)headsCount / num;
+    return ratio;
+}
+
+string result = TossCoin();
+Console.WriteLine("Resultado: {0}", result);
+Console.WriteLine();
+
+Console.WriteLine("*****[ TossMultipleCoins(int num) ]*****");
+int num = 15;
+double ratio = TossMultipleCoins(num);
+Console.WriteLine("Relación entre caras y lanzamientos: " + ratio);
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("********** Nombres **********");
+
+static List<string> Nombres()
+{
+    List<string> names = new List<string> { "Todd", "Tiffany", "Charlie", "Ginebra", "Sydney" };
+    List<string> filteredNames = new List<string>();
+
+    foreach (string name in names)
+    {
+        if (name.Length > 5)
+        {
+            filteredNames.Add(name);
+        }
+    }
+
+    return filteredNames;
+}
+
+static List<T> ShuffleList<T>(List<T> list)
+{
+    Random random = new Random();
+    List<T> shuffledList = new List<T>(list);
+    int n = shuffledList.Count;
+
+    while (n > 1)
+    {
+        n--;
+        int k = random.Next(n + 1);
+        T value = shuffledList[k];
+        shuffledList[k] = shuffledList[n];
+        shuffledList[n] = value;
+    }
+
+    return shuffledList;
+}
+
+List<string> names = Nombres();
+Console.WriteLine("Nombres con más de 5 caracteres:");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine();
+Console.WriteLine("Nombres barajados:");
+List<string> shuffledNames = ShuffleList(names);
+foreach (string name in shuffledNames)
+{
+    Console.WriteLine(name);
+}
---
+++ README.md
@@ -0,0 +1,47 @@
+# Puzzles
+
+Repositorio publico para el bootcamp de Coding Dojo.
+.
+<h3>Objetivo:</h3>
+    <li>Crear funciones utilizando la sintaxis de C#.​​</li>
+    <li>Implementar la lógica de programación para resolver problemas utilizando la sintaxis de C# en una aplicación de consola.​</li>
+    <li>Demostrar cómo se puede utilizar la función Aleatorio para conseguir varios efectos, como la generación de números aleatorios, resultados de cara o cruz, y barajar conjuntos de datos.​</li>
+
+<h3>Instrucciones</h3>
+
+<p>Completa todas las partes obligatorias de la tarea. Si puedes hacerlo en menos de 90 minutos, vuelve atrás y trabaja en las partes opcionales. Si te encuentras trabajando en esta tarea durante más de 2 horas, por favor, pide ayuda a un compañero o a un instructor para que no te retrases en el curso desde el principio.​​<p>
+
+<h2>Matriz Aleatoria </h2>
+<h4>Se requiere: </h4><p>Crear una función llamada RandomArray() que devuelva una matriz de enteros​</p>
+<ul>
+    <li>Crea una matriz vacía que contenga 10 valores enteros.​</li>
+    <li>Recorre la matriz y asigna a cada índice un valor entero aleatorio entre 5 y 25.​</li>
+    <li>Imprime los valores mínimo y máximo de la matriz.​</li>
+    <li>Imprime la suma de todos los valores.​</li>
+ </ul>
+ 
+<h2>Lanzamiento de Moneda</h2>
+
+<h4>Se requiere: <p>Crear una función llamada TossCoin() que devuelva una cadena. ​</p>
+<ul>
+    <li>Que la función imprima "¡Tirando una moneda!".​</li>
+    <li>Lanzar una moneda al azar con un resultado que señale cualquiera de las dos caras de la moneda.​</li>
+    <li>Haz que la función imprima "Cara" o "Cruz".​</li>
+    <li>Por último, devuelve el resultado.​</li>
+</ul>
+
+<h4>Opcional:</h4>
+<p>Crea otra función llamada TossMultipleCoins(int num) que devuelva un Doble. ​</p>
+<ul>
+    <li>Haz que la función llame a la función tossCoin varias veces en función del valor num.​</li>
+    <li>Haz que la función devuelva un Doble que refleje la relación entre el lanzamiento de cabezas y el lanzamiento total.​</li>
+</ul>
+
+<h2>Nombres</h2>
+<p>Construye una función Nombres que devuelva una lista de cadenas. En esta función:​<br></p><h4>Se requiere: </h4>
+<ul>
+    <li>Crea una lista con los valores Todd, Tiffany, Charlie, Ginebra, Sydney.</li>
+    <li>Devuelve una lista que solo incluya nombres de más de 5 caracteres.​</li>
+ </ul><h4>Opcional:</h4><ul> 
+    <li>Baraja la lista e imprime los valores en el nuevo orden. ​</li>
+ </ul>
